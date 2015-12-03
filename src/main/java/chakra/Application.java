7161dd3ce82a6e6499315ff4bbec7fd1b6e6001d
nishant <nishant.singh87@gmail.com>
2015-12-03 19:27:36 +0530

    separate app and migration config.

--- src/main/java/chakra/Application.java
+++ src/main/java/chakra/Application.java
@@ -1,22 +1,11 @@
 package chakra;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
-    setupMigrations();
     SpringApplication.run(Application.class, args);
-  }
-
-  private static void setupMigrations(){
-    Flyway flyway = new Flyway();
-    flyway.setDataSource(
-        System.getenv("postgres://rfaozzwhaeirje:vRKdlkiGjstuKVOmx2y3BbMKo3@ec2-54-83-0-61.compute-1.amazonaws.com:5432/dfdekq48kqa5sj\n"),
-        System.getenv("rfaozzwhaeirje"),
-        System.getenv("vRKdlkiGjstuKVOmx2y3BbMKo3"));
-    flyway.migrate();
   }
 }
